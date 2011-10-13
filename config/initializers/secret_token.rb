188891b03bcd74650831006b9b353efa0657a9c5
ZIya <ziya@My-World.(none)>
2011-10-13 12:29:34 +0530

    More Mall first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoreMall::Application.config.secret_token = '3abd6e1d79861c2fd4ecd4b4020e16477f2f4a66e9264838a4741136ca79b7eafe1c44307802c4f29161d2220c1925a2a1d54838e7998566ce0896786be46d64'
